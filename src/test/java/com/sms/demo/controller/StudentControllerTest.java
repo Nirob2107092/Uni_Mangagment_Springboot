cd7e39f24975683fbe40b7244a7043f2f0d0000c
Rysul Nirob <rysulamannirob@gmail.com>
2026-02-15 01:30:09 +0600

    initial commit

---
+++ src/test/java/com/sms/demo/controller/StudentControllerTest.java
@@ -0,0 +1,32 @@
+package com.sms.demo.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentControllerTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void updateSelf() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
